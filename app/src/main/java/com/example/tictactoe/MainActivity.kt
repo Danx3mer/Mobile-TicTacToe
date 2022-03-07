49bd333a7d8dfe1890b185b5341e000463dbea4b
daniel <dashingdaniel05@gmail.com>
2022-03-07 18:42:13 -0500

    Commit 9: Organization and first click functionallity

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-
+    fun cellClick(v: View){
+      Log.i("Click!!!", v.id.toString())
     }
 }
